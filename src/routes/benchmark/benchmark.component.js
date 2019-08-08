943c9f56923b75f703665b3aca9a192d4e7e5d25
Przemek Kwiatkowski <pkwiatkowski@apptension.com>
2019-08-08 16:20:04 +0200

    Style benchmark route

--- src/routes/benchmark/benchmark.component.js
+++ src/routes/benchmark/benchmark.component.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Container } from './benchmark.styles';
-import { H2 } from '../../theme/typography';
+import { H1, H2 } from '../../theme/typography';
 import { StartButton } from '../../shared/components/startButton';
 import { SaveButton } from '../../shared/components/saveButton';
 import { AddSample } from '../../shared/components/addSample';
+import { PageContainer, Header, NavigationContainer, Content } from '../../theme/global';
+import { NavigationBackIcon, TestDescription } from './benchmark.styles';
 
 const Benchmark = props => {
   const handleClick = () => {
@@ -13,15 +14,20 @@ const Benchmark = props => {
   };
 
   return (
-    <Container>
-      <button type="button" onClick={handleClick}>
-        go back
-      </button>
-      <H2>Test</H2>
-      <StartButton />
-      {props.isActive && props.startedAt && <AddSample />}
-      {props.isActive && <SaveButton />}
-    </Container>
+    <PageContainer>
+      <Header>
+        <NavigationBackIcon onClick={handleClick} />
+        <H1>Title of the test</H1>
+        <H2>benchmark</H2>
+      </Header>
+      <NavigationContainer />
+      <Content>
+        <TestDescription />
+        <StartButton />
+        {props.isActive && props.startedAt && <AddSample />}
+        {props.isActive && <SaveButton />}
+      </Content>
+    </PageContainer>
   );
 };
 
