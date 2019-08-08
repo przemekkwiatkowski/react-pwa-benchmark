b02ba90761e7e1775c0ede20db8eda2fbf2a9bac
Przemek Kwiatkowski <pkwiatkowski@apptension.com>
2019-08-08 13:44:47 +0200

    Add page structure

--- src/routes/home/home.component.js
+++ src/routes/home/home.component.js
@@ -5,8 +5,9 @@ import Helmet from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 
 import messages from './home.messages';
-import { Container, BenchmarksContainer } from './home.styles';
+import { BenchmarksContainer } from './home.styles';
 import { H1 } from '../../theme/typography';
+import { PageContainer, Header, NavigationContainer, Content } from '../../theme/global';
 import { BenchmarkTile } from '../../shared/components/benchmarkTile';
 
 export class Home extends PureComponent {
@@ -16,18 +17,23 @@ export class Home extends PureComponent {
 
   render() {
     return (
-      <Container>
+      <PageContainer>
         <Helmet title={this.props.intl.formatMessage(messages.pageTitle)} />
-        <H1>
-          <FormattedMessage {...messages.welcome} />
-        </H1>
-        <BenchmarksContainer>
-          <BenchmarkTile title="Test 1" link="/1" />
-          <BenchmarkTile title="Test 2" link="/2" />
-          <BenchmarkTile title="Test 3" link="/3" />
-          <BenchmarkTile title="Test 4" link="/4" />
-        </BenchmarksContainer>
-      </Container>
+        <Header>
+          <H1>
+            <FormattedMessage {...messages.welcome} />
+          </H1>
+        </Header>
+        <NavigationContainer />
+        <Content>
+          <BenchmarksContainer>
+            <BenchmarkTile title="Test 1" link="/1" />
+            <BenchmarkTile title="Test 2" link="/2" />
+            <BenchmarkTile title="Test 3" link="/3" />
+            <BenchmarkTile title="Test 4" link="/4" />
+          </BenchmarksContainer>
+        </Content>
+      </PageContainer>
     );
   }
 }
